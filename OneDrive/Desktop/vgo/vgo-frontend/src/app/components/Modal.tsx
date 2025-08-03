1dea813db9ae394d5fbd542ff8bc9e1a7ea58fc7
KingReevan <reevandmellO@gmail.com>
2025-08-03 19:52:21 +0530

    Improved functionality of the leaderboard

--- OneDrive/Desktop/vgo/vgo-frontend/src/app/components/Modal.tsx
+++ OneDrive/Desktop/vgo/vgo-frontend/src/app/components/Modal.tsx
@@ -1,6 +1,8 @@
 'use client'
-import { ReactNode } from 'react'
+
+import { useEffect, useRef } from 'react'
 import { X } from 'lucide-react'
+import type { ReactNode } from 'react'
 
 type ModalProps = {
   open: boolean
@@ -10,14 +12,35 @@ type ModalProps = {
 }
 
 export default function Modal({ open, onClose, title, children }: ModalProps) {
+  const modalRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        onClose()
+      }
+    }
+
+    if (open) {
+      document.addEventListener('mousedown', handleClickOutside)
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [open, onClose])
+
   if (!open) return null
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 backdrop-blur-sm">
-      <div className="bg-gray-900 text-white rounded-xl max-w-3xl w-full max-h-[80vh] overflow-y-auto shadow-[0_0_15px_2px_rgba(255,0,0,0.4)] border border-red-500 p-6 relative">
+      <div
+        ref={modalRef}
+        className="bg-gray-900 text-white rounded-xl max-w-3xl w-full max-h-[80vh] overflow-y-auto shadow-[0_0_15px_2px_rgba(255,0,0,0.4)] border border-red-500 p-6 relative"
+      >
         <button
           onClick={onClose}
-          className="absolute top-3 right-3 text-white hover:text-red-500"
+          className="absolute top-3 right-3 text-white hover:text-red-500 hover:text cursor-pointer"
         >
           <X size={24} />
         </button>
