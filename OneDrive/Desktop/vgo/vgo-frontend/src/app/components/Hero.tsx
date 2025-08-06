ed9d0a6dfe2043f7b13011c6e1b15c2e47cd4e45
KingReevan <reevandmellO@gmail.com>
2025-08-06 21:13:45 +0530

    Made hero section a bit more responsive

--- OneDrive/Desktop/vgo/vgo-frontend/src/app/components/Hero.tsx
+++ OneDrive/Desktop/vgo/vgo-frontend/src/app/components/Hero.tsx
@@ -6,7 +6,7 @@ import { LightningBoltIcon, GaugeIcon, ShieldIcon, PersonIcon, MapPinIcon, LoopI
 export default function Hero() {
   return (
     <section
-        className="relative flex flex-col items-center justify-center text-center space-y-6 min-h-[92vh] bg-cover bg-center bg-no-repeat"
+        className="relative flex flex-col items-center justify-center text-center space-y-6 sm:space-y-8 md:space-y-10 min-h-[85vh] sm:min-h-[92vh] bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "url('/hero-bg.jpeg')" }}
     >
         {/* Optional: dark overlay */}
@@ -14,14 +14,14 @@ export default function Hero() {
         <div className="absolute inset-0 bg-black/55 backdrop-blur-[2px] z-0" />
         
         {/* Hero Content */}
-        <div className="relative z-10 px-4">
-        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl lg:text-7xl text-primary">
+        <div className="relative z-10 px-4 sm:px-6 lg:px-8">
+        <h1 className="font-bold tracking-tight text-primary text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
             Experience the Thrill of VGO Racing
         </h1>
-        <p className="text-lg max-w-3xl mt-4 text-primary mx-auto">
+        <p className="mt-4 sm:mt-6 md:mt-8 text-primary mx-auto text-sm sm:text-base md:text-lg lg:text-xl max-w-md sm:max-w-xl md:max-w-2xl">
             Book your next go-karting adventure now and feel the adrenaline rush like never before!
         </p>
-        <div className="flex gap-4 justify-center mt-6">
+        <div className="flex gap-4 justify-center mt-4 sm:mt-6 md:mt-8">
             <Button asChild variant="default" size="lg" className="transform transition-transform duration-300 hover:scale-110">
             <Link href="/book" className="text-black w-36 h-12 flex items-center justify-center transition-all duration-300">Book Now</Link>
             </Button>
